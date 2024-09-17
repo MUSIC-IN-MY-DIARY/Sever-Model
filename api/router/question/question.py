be6a3cca889eae2b605f43bf4ad93afb4ee6dd4f
UICHEOL-HWANG <icuchoel@gmail.com>
2024-09-17 15:28:48 +0900

    Feat: question API 구축 및 스키마 추가

---
+++ api/router/question/question.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+# default Settings
+
+from .question_schemas import *
+# Modules
+
+question_router = APIRouter(
+    prefix="/question",
+    tags=["question"],
+    responses={404: {"description": "Not found"}},
+)
+
+# 기본 프레픽스, /question 이라는 엔드포인트로 띄워자게    됨
+
+@question_router.post("/", response_model=QuestionSchema)
+async def read_root():
+    return "hello"
+
+
+
