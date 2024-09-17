63fae8f2fb22a8b7db24103356ec6c4f9663f60b
UICHEOL-HWANG <icuchoel@gmail.com>
2024-09-17 15:02:39 +0900

    Feat: FastAPI 1차 구축

--- api/main.py
+++ api/main.py
@@ -1,3 +1,22 @@
-from config.models import test
+import uvicorn
 
-print(test())
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# default Settings for Fastapi
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+async def testing_get():
+    return {"say" : "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
